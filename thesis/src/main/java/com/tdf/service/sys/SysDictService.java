bf8f0b198bb4dd57add99ea2b32176cc27595126
feifeimeier <feifeimeier@aliyun.com>
2019-09-16 10:59:07 +0800

    no message

--- thesis/src/main/java/com/tdf/service/sys/SysDictService.java
+++ thesis/src/main/java/com/tdf/service/sys/SysDictService.java
@@ -25,9 +25,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(
-        rollbackForClassName = {"Exception.class"}
-)
+@Transactional(rollbackForClassName = {"Exception.class"})
 public class SysDictService {
     @Autowired
     private SysDictMapper sysDictMapper;
